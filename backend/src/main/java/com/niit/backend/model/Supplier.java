6a1938eb4a1fb5d031b908833277c4be63f2b7b2
MugundanSrinivasan <mugundan.531996@gmail.com>
2018-06-28 14:58:02 +0530

    This is my second commit

---
+++ backend/src/main/java/com/niit/backend/model/Supplier.java
@@ -0,0 +1,22 @@
+package com.niit.backend.model;
+
+public class Supplier {
+	
+	String name;
+	int supid;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSupid() {
+		return supid;
+	}
+	public void setSupid(int supid) {
+		this.supid = supid;
+	}
+	
+	
+
+}
